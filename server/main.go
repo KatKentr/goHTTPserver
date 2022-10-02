e2512e4db628ac584823b595df0d7a92d9c5d477
KatKentr <kkentroti@gmail.com>
2022-10-02 21:39:02 +0300

    implementation with a closure to utilize the same fibonacci function with different inputs

--- server/main.go
+++ server/main.go
@@ -14,9 +14,17 @@ import (
 
 )
 
-func Fibonacci10(w http.ResponseWriter, r *http.Request) {
+//returns the actual HTTP handler function
+func Fibonacci(number int) func(w http.ResponseWriter, r *http.Request) {
+
+//error handling sould be normally added: if number ==nil ...
+
+  return func(w http.ResponseWriter, r *http.Request){
+  
 	// return the 10th Fibonacci number in the response payload
-	fmt.Fprintf(w, "%d", myFunctions.Fibonacci(10))
+	fmt.Fprintf(w, "%d", myFunctions.Fibonacci(number))
+  }
+
 }
 
 
@@ -32,18 +40,19 @@ func main(){
     //different resource paths
     mux := http.NewServeMux()
     
-    //tell it to call the Fibonacci10() function
-    //when someone requests the resource path `/fibonacci10`
-    
-    mux.HandleFunc("/fibonacci10",Fibonacci10)
+    //tell it to call the Fibonacci(number) function
+    //when someone requests the resource path `/fibonacci10` or `/fibonacci20`  or `/fibonacci30`
+    mux.HandleFunc("/fibonacci10",Fibonacci(10))
+    mux.HandleFunc("/fibonacci20",Fibonacci(20))
+    mux.HandleFunc("/fibonacci30",Fibonacci(30))
+     
     
     
     
      //start the web server using the mux as the root handler,
     //and report any errors that occur.
     //the ListenAndServe() function will block so
-    //this program will continue to run until killed
-        
+    //this program will continue to run until killed        
     log.Printf("server is listening at %s...", addr)
     log.Fatal(http.ListenAndServe(addr, mux))
     
